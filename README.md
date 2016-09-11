8054e876bbe1f28a9104f6da99eae5916e02929b
Aitter <935486956@qq.com>
2016-09-12 00:46:14 +0800

    create exmples

--- README.md
+++ README.md
@@ -15,59 +15,71 @@
 
 ### 初始化REM
 
-```
-mTools.initREM();
-```
+根据设计稿的宽度生成REM的大小，如设计稿是750大小，那么页面上的尺寸就可以完成按照设计稿中的尺寸来写成px了，然后借助px2rem 的postcss插件将px转成rem单位就可自适用各种移动端屏幕了
 
+```javascript
+mTools.initREM(size); //size: 设计稿的宽度（默认为750）
+window.px2rem(75) = 1;
+window.rem2px(1) = 75;
+```
 
 ### 检测浏览器信息
 
-```
-mTools.browser();
-//
+```javascript
+mTools.browser;
+//{name: "Chrome", isChrome: true, version: "50.0.2661.75"}
 ```
 
 ### 检测系统平台
 
-```
-mTools.os();
-//
+```javascript
+mTools.os;
+//{name: "iPhone", isIPhone: true, isIPad: false, isIOS: true, version: "9.1"}
 ```
 
 ### 是否支持Webp图片
 
-```
+```javascript
 mTools.supportWebp(function(isSupport){
-
+  isSupport ? renderWebpImgs() : renderNormalImgs();
 });
 ```
 
 ### 是否在线
 
-```
+页面在第一次加载时，网络正常，是不会触发 `online` 事件的，只有当网络断开又连接时，才会同时触发 `online` 和 `offline` 事件
+
+```javascript
+//new mTools.Network(onlineFn, offlineFn)
 new mTools.Network(function(){
-
+  document.getElementById('networkTip').style.display='block';
 }, function(){
-
+  document.getElementById('networkTip').style.display='none';
 })
 ```
 
 ### 页面是否可见
 
-```
-mTools.pageVisiblity(function(){}, function(){})
+```javascript
+//mTools.pageVisiblity(visibleFn, hideFn)
+
+mTools.pageVisiblity(function(){
+  console.log('页面可见', Date())
+}, function(){
+  console.log('页面不可见', Date())
+})
 ```
 
 ### 打印错误日志
 
-```
-window.onerror=function(err){ mTools.log(err) }
+```javascript
+window.onerror = function(err){ mTools.log(err) }
 ```
 
 ### 获取 requestAnimationFrame 对象
 
-```
+```javascript
 mTools.rAF(function(){
-
+//实时更新DOM
 });
 ```
--- mTools.js
+++ mTools.js
@@ -8,14 +8,15 @@
 /**
  * [mTools.js 移动端工具类]
  * author: 935486956@qq.com
- * github:
+ * github: https://github.com/git-lt/mTools.js
  * version: v0.0.1
  */
 ;(function(window, document, undefined){
   var mTools = {}, win=window, doc = document, ua = window.navigator.userAgent;
 
-  mTools.initREM = function(){
+  mTools.initREM = function(size){
     var docEl = doc.documentElement;
+    var size = size || 750;
     var resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize';
     win.docH = docEl.clientHeight;
     win.docW = docEl.clientWidth;
@@ -24,13 +25,12 @@
     win.rem2px = function(n){ return parseFloat(n) * win.rem; }
     docEl.style.opacity=0;
     docEl.setAttribute('data-dpr', win.dpr);
-
     var recalc = function () {
           win.docH = docEl.clientHeight;
           win.docW = docEl.clientWidth;
           var clientWidth = win.docW || 320;
           var docCls = docEl.classList;
-          var fontSize = (clientWidth < 320 ? 320 : clientWidth > 750 ? 750 : clientWidth)/10;
+          var fontSize = (clientWidth < 320 ? 320 : clientWidth > size ? size : clientWidth)/10;
           docEl.style.fontSize = fontSize + 'px';
           win.rem = fontSize;
           docEl.style.opacity = 1;
@@ -179,7 +179,7 @@
     if(!oLogBox){
       oLogBox = doc.createElement('div');
       oLogBox.id = 'logBox';
-      oLogBox.style.cssText = "position:fixed; bottom:0; left:0; right:0; top:auto; pading:10px;background:#333;color:#red; word-break:break-all;"
+      oLogBox.style.cssText = "position: fixed; bottom: 0px; left: 0px; right: 0px; top: auto; word-break: break-all; background: rgb(249, 204, 204); font-size: 14px; color: red; padding: 20px 10px;"
       doc.body.appendChild(oLogBox);
     }
     oLogBox.innerHTML += errmsg+(data && JSON.stringify(data) || '')+'<br/>';
@@ -213,13 +213,13 @@
   mTools.Network = function(onlineFn, offlineFn){
     this.onlineFn = onlineFn;
     this.offlineFn = offlineFn;
-    this.isOnline = win.navigator.online;
+    this.isOnline = win.navigator.onLine;
     this.init();
   }
   mTools.Network.prototype.init = function(){
     var _this = this;
       win.addEventListener("online",function(){
-           that.onlineFn() ;
+           _this.onlineFn() ;
        }, true) ;
        win.addEventListener("offline",function(){
            _this.offlineFn() ;
@@ -228,13 +228,14 @@
 
   // 使用场景：更新聊天在线状态、视频播放与暂停、登录状态同步
   mTools.pageVisiblity = function(visibleFn, hideFn){
-    doc.addEventListener('visibilitychange webkitvisibilitychange', function() {
+    var eName =  typeof doc['hidden'] === 'undefined' ? 'webkitvisibilitychange' : 'visibilitychange';
+    doc.addEventListener(eName, function() {
       if (doc.hidden) {
         hideFn && hideFn();
       } else {
         visibleFn && visibleFn();
       }
-    });
+    }, false);
   }
 
   mTools.supportWebp();
